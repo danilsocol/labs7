4141290fb8a368b72c9c8d0f3f099dcf41858048
danilsocol <danilsocol-2002@mail.ru>
2020-10-25 20:26:49 +0500

    1

---
+++ lab7/lab7/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string example = ReadFile();
+
+            example = DeleteSpace(example);
+
+           string[] str = Reworking(example);
+
+            if (check(str))
+            {
+                Refusal();
+            }
+
+
+           string result =  SelectingAnAction(str);
+
+            Write(result, example);
+
+        }
+
+       static string ReadFile()
+        {
+            return File.ReadAllText("input.txt");
+            
+        }
+
+        static string DeleteSpace(string example)
+        {
+            return Regex.Replace(example, @"\s+", " ");
+        }
+
+        static string[] Reworking(string example)
+        {
+
+            return example.Split(new char[] { ' ' });
+
+        }
+
+        static bool check(string[] str)
+        {
+            return str[1] != "+" && str[1] != "-" && str[1] != "/" && str[1] != "*";
+        }
+
+        static string SelectingAnAction(string[] str)
+        {
+            int oneNum = Convert.ToInt32(str[0]);
+            int twoNum = Convert.ToInt32(str[2]);
+            double result;
+
+            switch (str[1]) 
+            {
+
+                case "+":
+                    result = (double) oneNum + twoNum;
+                    break;
+
+                case "-":
+                    result = (double) oneNum - twoNum;
+                    break;
+
+                case "/":
+                    if (twoNum == 0)
+                        Refusal();
+                    result = (double) oneNum / twoNum;
+                    break;
+
+                case "*":
+                    result = (double) oneNum * twoNum;
+                    break;
+
+                default:
+                    return "?";
+                    
+            }
+            return Convert.ToString(Math.Round(result,2));
+
+        }
+
+        static void Write(string result,string example)
+        {
+            Console.WriteLine($"{ example} = { result}");
+            File.WriteAllText("output.txt",$"{ example} = { result}");
+        }
+
+        static void Refusal()
+        {
+            Console.WriteLine("Недопустимая операция");
+            return;
+        }
+    }
+}
